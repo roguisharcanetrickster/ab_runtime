49529bf639f3e0619fade990d47412b69ff0ddfe
Johnny Hausman <johnny@appdevdesigns.net>
2019-11-20 23:01:02 +0700

    [fix] added redis as a discovery service for cote messaging  +  eslint updates.

--- UP.sh
+++ UP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 Dev=$1
-File="docker-compose.dev.yml"
+File="docker-compose.yml"
 if [ -z "$Dev" ]
 then
-	File="docker-compose.yml"
+	File="docker-compose.dev.yml"
 fi
 docker stack deploy -c $File ab
 ./logs.js
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const net = require("net");
 const fs = require("fs");
 const shell = require("shelljs");
-const async = require("async");
+// const async = require("async");
 const AB = require("ab-utils");
 
 // const SOCKETFILE = "1338";
@@ -12,7 +12,7 @@ let server = null;
 let currStream = null;
 
 var isReportRunning = false;
-var checkID = null;
+// var checkID = null;
 
 // Net
 // Connections to the running bot_manager in our swarm.
@@ -23,26 +23,26 @@ var SOCKETFILE = "/tmp/ab.sock";
 var PORT = "1338";
 var ACCESSTOKEN = "ThereIsN0Sp00n";
 if (
-  !config.hostConnection.sharedSock ||
-  !config.hostConnection.sharedSock.path
+    !config.hostConnection.sharedSock ||
+    !config.hostConnection.sharedSock.path
 ) {
-  isSockConnection = false;
-  if (
-    !config.hostConnection.tcp ||
-    !config.hostConnection.tcp.port ||
-    !config.hostConnection.tcp.accessToken
-  ) {
-    console.error(
-      "ERROR: config/local.js must specify a bot_manager.hostConnection."
-    );
-    process.exit(1);
-  }
+    isSockConnection = false;
+    if (
+        !config.hostConnection.tcp ||
+        !config.hostConnection.tcp.port ||
+        !config.hostConnection.tcp.accessToken
+    ) {
+        console.error(
+            "ERROR: config/local.js must specify a bot_manager.hostConnection."
+        );
+        process.exit(1);
+    }
 }
 if (isSockConnection) {
-  SOCKETFILE = config.hostConnection.sharedSock.path;
+    SOCKETFILE = config.hostConnection.sharedSock.path;
 } else {
-  PORT = config.hostConnection.tcp.port;
-  ACCESSTOKEN = config.hostConnection.tcp.accessToken;
+    PORT = config.hostConnection.tcp.port;
+    ACCESSTOKEN = config.hostConnection.tcp.accessToken;
 }
 
 /**
@@ -61,110 +61,124 @@ var parseLine = /(\w+)\s+(\w+)\s+\w+\s+0\/\d+\s+([\w\/:-]+)/g;
 // h8bx5qewkeaq        ab_apiSails         replicated          1/1                 skipdaddy/ab-api-sails:master   *:1337->1337/tcp
 // parseLine 1:ID,  2:Name,  3:Image
 function checkRunning() {
-  // make sure our connection to our botManager is ok before trying:
-  if (currStream && !currStream.destroyed) {
-    shell.exec("docker service ls", (code, stdout, stderr) => {
-      if (code == 0) {
-        if (stdout.search(downCheck) == -1) {
-          if (isReportRunning) {
-            console.log("... looks like server is running.");
-            currStream.write("__running");
-            isReportRunning = false;
-          }
-        } else {
-          var lines = stdout.split("\n");
-          var offlineContainers = [];
-          lines.forEach(l => {
-            var match = parseLine.exec(l);
-            if (match) {
-              offlineContainers.push(`${match[2]}`); // ${match[2]}(${match[3]})
+    // make sure our connection to our botManager is ok before trying:
+    if (currStream && !currStream.destroyed) {
+        shell.exec("docker service ls", (code, stdout /* , stderr */) => {
+            if (code == 0) {
+                if (stdout.search(downCheck) == -1) {
+                    if (isReportRunning) {
+                        console.log("... looks like server is running.");
+                        if (currStream) {
+                            currStream.write("__running");
+                        }
+
+                        isReportRunning = false;
+                    }
+                } else {
+                    var lines = stdout.split("\n");
+                    var offlineContainers = [];
+                    lines.forEach((l) => {
+                        var match = parseLine.exec(l);
+                        if (match) {
+                            offlineContainers.push(`${match[2]}`); // ${match[2]}(${match[3]})
+                        }
+                    });
+                    if (offlineContainers.length > 0) {
+                        if (currStream) {
+                            currStream.write(
+                                "__offline:" + offlineContainers.join(",")
+                            );
+                        }
+                        isReportRunning = true;
+                    }
+                }
             }
-          });
-          if (offlineContainers.length > 0) {
-            currStream.write("__offline:" + offlineContainers.join(","));
-            isReportRunning = true;
-          }
-        }
-      }
-    });
-  } else {
-    console.log("connection to botManager is not active.");
-  }
+        });
+    } else {
+        console.log("connection to botManager is not active.");
+    }
 }
 
 function createServer(socket) {
-  console.log("Creating server.");
-  var server = net
-    .createServer(function(stream) {
-      console.log("Connection acknowledged.");
-      currStream = stream;
+    console.log("Creating server.");
+    var server = net
+        .createServer(function(stream) {
+            console.log("Connection acknowledged.");
+            currStream = stream;
 
-      stream._sentValidToken = false;
+            stream._sentValidToken = false;
 
-      // Store all connections so we can terminate them if the server closes.
-      // An object is better than an array for these.
-      var self = Date.now();
-      connections[self] = stream;
-      stream.on("end", function() {
-        console.log("Client disconnected.");
-        delete connections[self];
-      });
+            // Store all connections so we can terminate them if the server closes.
+            // An object is better than an array for these.
+            var self = Date.now();
+            connections[self] = stream;
+            stream.on("end", function() {
+                console.log("Client disconnected.");
+                delete connections[self];
+            });
 
-      // Messages are buffers. use toString
-      stream.on("data", function(msg) {
-        msg = msg.toString();
+            // Messages are buffers. use toString
+            stream.on("data", function(msg) {
+                msg = msg.toString();
 
-        console.log("Client:", msg);
+                console.log("Client:", msg);
 
-        // verify access token if necessary
-        if (!isSockConnection && !stream._sentValidToken) {
-          if (msg == ACCESSTOKEN) {
-            stream._sentValidToken = true;
-            console.log("... accessToken accepted.");
-          } else {
-            console.log("... accessToken REJECTED!");
-          }
-        } else {
-          // else execute a command:
-          shell.exec(msg, (code, stdout, stderr) => {
-            // console.log("code:", code);
-            // console.log("stdout:", stdout);
-            // console.log("stderr:", stderr);
+                // verify access token if necessary
+                if (!isSockConnection && !stream._sentValidToken) {
+                    if (msg == ACCESSTOKEN) {
+                        stream._sentValidToken = true;
+                        console.log("... accessToken accepted.");
+                    } else {
+                        console.log("... accessToken REJECTED!");
+                    }
+                } else {
+                    // else execute a command:
+                    shell.exec(msg, (/* code, stdout, stderr */) => {
+                        // console.log("code:", code);
+                        // console.log("stdout:", stdout);
+                        // console.log("stderr:", stderr);
 
-            // indicate we need to report when everything is running
-            isReportRunning = true;
-            stream.write("__done"); // command is done
-          });
-        }
-      });
-    })
-    .listen(socket)
-    .on("connection", function(socket) {
-      console.log("Client connected.");
-    });
-  return server;
+                        // indicate we need to report when everything is running
+                        isReportRunning = true;
+                        stream.write("__done"); // command is done
+                    });
+                }
+            });
+
+            stream.on("error", function(err) {
+                console.log(".onError():", err);
+                if (currStream == stream) {
+                    currStream = null;
+                }
+            });
+        })
+        .listen(socket)
+        .on("connection", function(/* socket */) {
+            console.log("Client connected.");
+        });
+    return server;
 }
 
 // manage unix socket:
 console.log("Checking for leftover socket.");
-fs.stat(SOCKETFILE, function(err, stats) {
-  if (err) {
-    // start server
-    console.log("No leftover socket found.");
-    server = createServer(isSockConnection ? SOCKETFILE : PORT);
-    return;
-  }
-  // remove file then start server
-  console.log("Removing leftover socket.");
-  fs.unlink(SOCKETFILE, function(err) {
+fs.stat(SOCKETFILE, function(err /*, stats */) {
     if (err) {
-      // This should never happen.
-      console.error(err);
-      process.exit(0);
+        // start server
+        console.log("No leftover socket found.");
+        server = createServer(isSockConnection ? SOCKETFILE : PORT);
+        return;
     }
-    server = createServer(isSockConnection ? SOCKETFILE : PORT);
-    return;
-  });
+    // remove file then start server
+    console.log("Removing leftover socket.");
+    fs.unlink(SOCKETFILE, function(err) {
+        if (err) {
+            // This should never happen.
+            console.error(err);
+            process.exit(0);
+        }
+        server = createServer(isSockConnection ? SOCKETFILE : PORT);
+        return;
+    });
 });
 
 // now setup our checkRunning() operation
