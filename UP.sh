d5833db899b33001bf57b3f8050924bfa3ba10b3
nate <7259@pm.me>
2021-11-10 13:59:06 +0700

    [add] override option to make the main stack testable

--- UP.sh
+++ UP.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-Dev=$1
+Dev=
+Test=
+# Read flags -d for dev, -t for test
+while getopts dt name
+do
+    case $name in
+    d)    Dev="true";;
+    t)    Test="true";;
+    esac
+done
 File="docker-compose.yml"
-if [ -z "$Dev" ]
+TestOveride=""
+if [[ -n $Dev ]]
 then
 	File="docker-compose.dev.yml"
 fi
-nohup node ab_system_monitor.js &> /dev/null & 
-docker stack deploy -c $File -c docker-compose.override.yml ab
+if [[ -n $Test ]]
+then
+	TestOveride="-c ./test/setup/ci-test.overide.yml"
+fi
+nohup node ab_system_monitor.js &> /dev/null &
+docker stack deploy -c $File -c docker-compose.override.yml $TestOveride ab
 ./logs.js
