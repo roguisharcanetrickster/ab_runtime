754ce806d691e32d5d1f284e5b7f0c3fbb6b9476
Johnny Hausman <johnny@hausmanfamily.com>
2021-11-15 14:28:02 +0700

    [fix] upgrading to latest sails@1.5.0 requires an "appbuilder" db created thanks to a stupid/new connection pool feature

--- mysql/init/01-CreateDBs.sql
+++ mysql/init/01-CreateDBs.sql
@@ -1,5 +1,5 @@
 # create databases
-# CREATE DATABASE IF NOT EXISTS `appbuilder` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `appbuilder` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS `appbuilder-admin` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 # create root user and grant rights
