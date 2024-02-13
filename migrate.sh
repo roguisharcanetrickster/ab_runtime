0955a2c7efec1123663726afa4ceaeb7ff1012a4
Johnny Hausman <johnny@hausmanfamily.com>
2024-02-13 16:33:15 -0600

    [add] Include a -t flag for migrate.sh to run migrations on the testing environment.

--- migrate.sh
+++ migrate.sh
@@ -3,23 +3,36 @@
 # Uses the AB Migration Manager to update every tenant DB to the latest
 # schema.
 
-
 # Import ENV variables
 set -o allexport
 source .env
 set +o allexport
 
+Test=
+# Read flags -t for test
+while getopts dtq name
+do
+    case $name in
+    t)    Test="true";;
+    esac
+done
+
+STACK="${STACKNAME}"
+if [[ -n $Test ]]
+then
+  STACK="${CYPRESS_STACK}"
+fi
 
 ${PLATFORM} pull docker.io/digiserve/ab-migration-manager:master
 
 SCRIPT_DIR=`dirname -- $0`
-ID=`${PLATFORM} container ls -f name=${STACKNAME}_api_sails`
+ID=`${PLATFORM} container ls -f name=${STACK}_api_sails`
 if [ "$ID" == "" ]
 then
-    echo "${STACKNAME} is not running"
+    echo "${STACK} is not running"
 else
     ${PLATFORM} run \
-        --network=${STACKNAME}_default \
+        --network=${STACK}_default \
         -e MYSQL_PASSWORD \
         digiserve/ab-migration-manager:master node app.js
 fi
