a63499e7b3cce032b8cd311678e6b08eec517438
Nate <10155226+nh758@users.noreply.github.com>
2024-03-15 12:39:37 +0700

    block outgoing sentry requests from cypress tests

--- test/e2e/cypress/support/e2e.js
+++ test/e2e/cypress/support/e2e.js
@@ -18,3 +18,8 @@ import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(() => {
+   // block/stub outgoing requests to sentry
+   cy.intercept({ hostname: /sentry\.io/ }, (req) => req.reply("success"));
+});
