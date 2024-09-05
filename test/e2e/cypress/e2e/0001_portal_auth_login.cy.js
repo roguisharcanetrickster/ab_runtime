aaed14e204344ee906e98952ae26eb6a99c4bbc2
nh758 <7259@pm.me>
2024-09-05 15:26:43 +0700

    add test case for reseting password

--- test/e2e/cypress/e2e/0001_portal_auth_login.cy.js
+++ test/e2e/cypress/e2e/0001_portal_auth_login.cy.js
@@ -1,6 +1,9 @@
 // Don't stop tests on uncaught errors
 Cypress.on("uncaught:exception", () => false);
 
+const USER_EMAIL = Cypress.env("USER_EMAIL");
+const USER_PASSWORD = Cypress.env("USER_PASSWORD");
+
 describe("Login Page", () => {
    before(() => {
       // make sure we have a clean slate before continuing.
@@ -13,7 +16,7 @@ describe("Login Page", () => {
 
    // 1) Successful Login
    it("loads the login page", () => {
-      Login(Cypress.env("USER_EMAIL"), Cypress.env("USER_PASSWORD"));
+      Login(USER_EMAIL, USER_PASSWORD);
 
       // warning should NOT exist
       cy.get("[data-cy=portal_auth_login_form_errormsg]").should("not.exist");
@@ -24,17 +27,71 @@ describe("Login Page", () => {
 
    // 2) Successful Logout
    it("Logging out, returns you to the Login Form:", () => {
-      Login(Cypress.env("USER_EMAIL"), Cypress.env("USER_PASSWORD"));
+      Login(USER_EMAIL, USER_PASSWORD);
       cy.get("[data-cy=user_icon]").should("be.visible").click();
       cy.get("[data-cy=user_logout]").should("be.visible").click();
    });
 
    // 3) Login Error Messages
    it("Displays Error Message when invalid Username / Password", () => {
-      Login(`a${Cypress.env("USER_EMAIL")}`, Cypress.env("USER_PASSWORD"));
+      Login(`a${USER_EMAIL}`, USER_PASSWORD);
 
       // now the warning should exist
       cy.get("[data-cy=portal_auth_login_form_errormsg]").should("exist");
+   });
+});
+
+describe("Reset Password", () => {
+   before(() => {
+      cy.ResetDB();
+   });
+   it("Can change a password", () => {
+      cy.visit("/");
+      cy.get('[data-cy="portal_auth_login_forgot"]')
+         .should("be.visible")
+         .click();
+      cy.get('[data-cy="portal_reset_request_email"]')
+         .should("be.visible")
+         .type(USER_EMAIL);
+      cy.get('[data-cy="portal_reset_request_send"]')
+         .should("be.visible")
+         .click();
+      // The user would now recieve an email with a link, including a site token
+      // we'll retrieve the generated token and create the link;
+      // Login to get token - don't use cy.AuthLogin since we won't reuse this
+      // session
+      cy.request("POST", "/auth/login", {
+         tenant: Cypress.env("TENANT"),
+         email: USER_EMAIL,
+         password: USER_PASSWORD,
+      })
+         .its("body")
+         .as("currentUser");
+      // reload to get a new ABFactory in the window (authenticated)
+      cy.visit("/");
+      cy.ModelFind("08826ac7-4b33-4745-a3d7-f7831ca4ff59", {}).then(
+         ({ data }) => {
+            const { token } = data[0];
+            // now that we have the token logout an simulate the link from email;
+            logOut();
+            cy.visit(`/auth/password/reset?a=${token}`);
+            const password = "newpassword";
+            cy.get('[data-cy="portal_reset_password_new"]')
+
+               .should("be.visible")
+               .type(password);
+            cy.get('[data-cy="portal_reset_password_confirm"]')
+               .should("be.visible")
+               .type(password);
+            cy.get('[data-cy="portal_reset_password_submit"]')
+               .should("be.visible")
+               .click();
+            logOut();
+            // Check the new passwor works
+            Login(USER_EMAIL, password);
+            cy.get("[data-cy=portal_work_menu_sidebar]").should("be.visible");
+         },
+      );
    });
 });
 
@@ -57,4 +114,9 @@ function Login(email, passowrd) {
    cy.get("input[data-cy=portal_auth_login_form_email]").type(email);
    cy.get("input[data-cy=portal_auth_login_form_password]").type(passowrd);
    cy.get("[data-cy=portal_auth_login_form_submit]").click();
+}
+
+function logOut() {
+   cy.get('[data-cy="user_icon"]').should("be.visible").click();
+   cy.get('[data-cy="user_logout"]').should("be.visible").click();
 }
